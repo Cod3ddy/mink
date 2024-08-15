46700ce629380088a2a535bfbba34505aa3d1d18
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-15 21:55:10 +0200

    feat: implement template cache func

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,44 @@
 package main
 
-import "html/template"
+import (
+	"html/template"
+	"io/fs"
+	"path/filepath"
+
+	"github.com/cod3ddy/mink/ui"
+)
+
+
+type templateData struct{
+	Form any
+}
 
 func newTemplateChache() (map[string]*template.Template, error){
+	// Init cahe map
+	cache := map[string]*template.Template{}
 
-	return nil, nil
+	pages, err := fs.Glob(ui.Files, "html/pages/*.html")
+	if err != nil{
+		return nil, err 
+	}
+
+
+	for _, page := range pages{
+		name := filepath.Base(page)
+
+		patterns  := []string{
+			"html/base.html",
+			page, 
+		}
+
+		ts, err := template.New(name).ParseFS(ui.Files, patterns...)
+
+		if err != nil{
+			return nil, err
+		}
+		
+		cache [name] = ts
+	}
+
+	return cache, nil
 }
