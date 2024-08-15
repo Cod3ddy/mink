4a742772ba9bf1556462620a09972448dddf4462
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-15 23:05:34 +0200

    feat: add shorten url endpoint

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,5 +13,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /ping", ping)
 
 	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("POST /url/shorten", app.shortenUrl)
 	return mux
 }
