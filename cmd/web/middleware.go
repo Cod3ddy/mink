ad68dad72269c2039197f27466a852bb0b1a94c2
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-16 17:07:04 +0200

    feat: fixed a typo in recoverPanic middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,14 +1,19 @@
 package main
 
-func (app *application) recoverPanic(next http.Handler) http.Handler{
+import (
+	"fmt"
+	"net/http"
+)
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		defer func (){
-			if err := recover(); err != nil{
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
 				w.Header().Set("Connection", "close")
 				app.serverError(w, r, fmt.Errorf("%s", err))
 			}
-		}
+		}()
 
 		next.ServeHTTP(w, r)
 	})
