64c8602cc4adb8bedf17ba1d3637c9eddbbcbffd
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-16 17:17:03 +0200

    feat: implement logrequest and common headers middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+func commonHeaders(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Server", "Go")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +21,20 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverError(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		var(
+			ip = r.RemoteAddr
+			proto = r.Proto
+			method = r.Method
+			uri = r.URL.RequestURI()
+		)
+		app.Logger.Info("received request","ip", ip, "proto", proto, "method", method, "uri", uri)
 
 		next.ServeHTTP(w, r)
 	})
